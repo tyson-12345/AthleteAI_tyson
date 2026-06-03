44c65b7eb423286579129f7264bcce92f527f7e4
oscar-chuang <oac60647@gmail.com>
2026-06-03 00:50:22 -0400

    Fix next.config: move serverExternalPackages to top level

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    serverExternalPackages: ["@mediapipe/pose"],
-  },
+  serverExternalPackages: ["@mediapipe/pose"],
 };
 
 export default nextConfig;
